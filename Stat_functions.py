85c6242598692697276b389d5e56c0c292e2cd04
Artiens <134687386+Artiens@users.noreply.github.com>
2023-12-03 01:04:18 +0100

    Add files via upload

--- Stat_functions.py
+++ Stat_functions.py
@@ -1,6 +1,25 @@
 import os
 import pandas as pd
 
+def winrate_first(data):
+    data = data.iloc[:, [0, -1]]
+    stock = []
+    for i in range(1,10):
+        cntw = 0
+        cntl = 0
+        cntd = 0
+        for index, ligne in data.iterrows():
+            if ligne['RESULT'] == 'win' and ligne['MOVE1']==i:
+                cntw = cntw + 1
+            if ligne['RESULT'] == 'loss' and ligne['MOVE1']==i:
+                cntl = cntl + 1
+            if ligne['RESULT'] == 'draw' and ligne['MOVE1']==i:
+                cntd = cntd + 1
+        stock.append((round(cntw/(cntw+cntl+cntd),3),round(cntl/(cntw+cntl+cntd),3),round(cntd/(cntw+cntl+cntd),3)))
+    return stock
+                
+
+
 def winrate(data):
     win = 0
     loss = 0
@@ -16,7 +35,7 @@ def finish(data, move):
     number_move = 0
     for index, row in data.iloc[1:].iterrows():
         
-        question = list(row).count('?')
+        question = list(row).count(0)
         if question == 9-move:
             number_move = number_move + 1
     return number_move/data.shape[0]
@@ -28,7 +47,23 @@ if __name__ == "__main__":
     chemin_fichier_csv = os.path.join(os.path.dirname(__file__), nom_fichier_csv)
     with open(chemin_fichier_csv, "r", newline="") as file:
         data = pd.read_csv(chemin_fichier_csv, delimiter=';', encoding='utf-8', header=0)
+        position_mapping = {
+            "?" : 0,
+            "(0, 0)": 1,
+            "(0, 1)": 2,
+            "(0, 2)": 3,
+            "(1, 0)": 4,
+            "(1, 1)": 5,
+            "(1, 2)": 6,
+            "(2, 0)": 7,
+            "(2, 1)": 8,
+            "(2, 2)": 9,
+        }
 
+        # Transformation des coordonnées en valeurs numériques
+        for col in data.keys():
+            if col != 'RESULT':
+                data[col] = [position_mapping[m] if m in position_mapping else position_mapping['?'] for m in data[col]]
         # Calculate the winrate.
         win, loss, draw = winrate(data)
         print(f"The probability of winning as the first player is {win}\nThe probability of losing as the first player is {loss}\nThe probability of a draw is {draw}\n")       
@@ -37,4 +72,8 @@ if __name__ == "__main__":
         for n in range(5,10):
             number_move = finish(data,n)
             print(f"The probability that a game finish in {n} moves is {number_move}.\n")
+        
+        # Calculate the winrate of a first move.
+        stock = winrate_first(data)
+        print(f"{stock}")
 
